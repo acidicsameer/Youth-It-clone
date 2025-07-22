e730dc905c79245b7787d6d9c526b4383cffdfb0
Sameer Sapkota <namrmc008@gmail.com>
2025-07-23 00:05:06 +0545

    ..

--- vite-project/src/components/SponserTeam.jsx
+++ vite-project/src/components/SponserTeam.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-
+import sagarmatha from '../assets/images/sagarmatha.png'
+import sdg_foundation from '../assets/images/sdg.png'
+import  ritxcollege from '../assets/images/ritx.png'
+import  makalu  from   '../assets/images/makalu.png      '
+import  purwanchal  from   '../assets/images/purwanchal.png'
 const SponserTeam = () => {
   return (
     <div className=' flex lg:justify-around justify-between flex-wrap  gap-y-1.5 lg:min-h-[120px]  border-2 border-sky-200 items-center p-[15px]'>
     <div className='lg:w-[150px]  lg:border-r-3  font-bold mb-2 ml-20  lg:m-0  border-black'>Our Trusted Organizations</div>
-    <div><img src="../assets/images/sdg.png"  className='lg:h-[80px] h-[50px] rounded-3xl' alt="" /></div>
-    <div><img src="/src/assets/images/ritx.png"  className='lg:h-[80px] h-[50px] rounded-3xl' alt="" /></div>
-    <div><img src="/src/assets/images/sagarmatha.png"  className='lg:h-[80px] h-[50px]  rounded-3xl' alt="" /></div>
-    <div><img src="/src/assets/images/makalu.png" alt="" className='lg:h-[80px] h-[50px]  rounded-3xl'  /></div>
-    <div><img src="/src/assets/images/home.png" alt=""  className='lg:h-[80px] h-[50px]  rounded-3xl' /></div>
-    <div><img src="/src/assets/images/sagarmatha.png" className='lg:h-[80px] h-[50px]  rounded-3xl'alt="" /></div>
+    <div><img src={sdg_foundation}  className='lg:h-[80px] h-[50px] rounded-3xl' alt="" /></div>
+    <div><img src={ritxcollege}  className='lg:h-[80px] h-[50px]  rounded-3xl' alt="" /></div>
+    <div><img src={makalu} alt="" className='lg:h-[80px] h-[50px]  rounded-3xl'  /></div>
+    <div><img src={purwanchal} alt=""  className='lg:h-[80px] h-[50px]  rounded-3xl' /></div>
+    <div><img src={sagarmatha} className='lg:h-[80px] h-[50px]  rounded-3xl'alt="" /></div>
 
   </div>
   )
